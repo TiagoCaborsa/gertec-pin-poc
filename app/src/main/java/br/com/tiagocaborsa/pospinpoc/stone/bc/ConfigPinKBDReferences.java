f230ce4c75c9f1275aa61b5cc828285437c13f41
TiagoCabral <tiago.gnr1985@gmail.com>
2019-08-27 14:35:59 -0300

    Creates callback to set pin referenes.

---
+++ app/src/main/java/br/com/tiagocaborsa/pospinpoc/stone/bc/ConfigPinKBDReferences.java
@@ -0,0 +1,7 @@
+package br.com.tiagocaborsa.pospinpoc.stone.bc;
+
+import java.io.Serializable;
+
+public interface ConfigPinKBDReferences extends Serializable {
+    void setPinKBDReferences(final PinKBDReferences pinKBDReferences);
+}
